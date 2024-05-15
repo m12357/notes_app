80dec33b015e4f7dec9667513f56953bf06d65c7
user <medhatmedhat532@gmail.com>
2024-05-15 18:55:20 +0300

    Custom App bar

---
+++ lib/views/widgets/custom_app_bar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'custom_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({Key? Key}) : super(key: Key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Text(
+          "Notes",
+          style: TextStyle(
+            fontSize: 28,
+          ),
+        ),
+        Spacer(),
+        CustomSearchIcon()
+      ],
+    );
+  }
+}
---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'custom_app_bar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({Key? Key}) : super(key: Key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child:  Column(children: [
+        SizedBox(
+          height: 50,
+        ),
+        CustomAppBar()
+      ]),
+    );
+  }
+}
